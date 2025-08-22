e4b8c33057db6c498668cc065863befe59b9b64a
konstantin <k35753367@gmail.com>
2025-08-22 13:28:20 +0300

    fix(tests): fixed tests by isort

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize(
